9e916d5c7e1fb7d2578e22d07b73689ecd867923
marti-oss <marti.serra.aguilera@gmail.com>
2022-06-07 23:37:33 +0200

    tema tokens i seguretat tancat m'havia deixat 10 fitxers, ara sí

---
+++ src/Controller/AdminUserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AdminUserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminUserController extends AbstractController
+{
+
+
+}
